c8a6e254f567a7fb8fe17fa44c61c3a9739179b5
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-04-02 19:08:07 +0700

    added username and auth feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users, controllers: {
+    :sessions => "users/sessions",
+    :registrations => "users/registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #
 end
